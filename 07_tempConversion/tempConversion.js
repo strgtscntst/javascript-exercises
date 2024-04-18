2e9a05ab573d269665e4bfdd8e971707c24188b2
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2024-04-18 16:28:39 -0400

    edit 07 solution for lesson
    Also edit 07 test file as per instructions.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperature) {
+  return Math.round(10*((temperature)-32) * (5/9))/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  return Math.round(10*(temperature * (9/5)+32))/10
 };
 
 // Do not edit below this line
