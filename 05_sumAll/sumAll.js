111e7de82e9f3da8aeddb3b00ff886ac6f94bb39
strgtscntst <66039310+strgtscntst@users.noreply.github.com>
2024-04-18 15:55:47 -0400

    edit 05 to include solution for lesson.
    Also modifies 05 test file to remove skips.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(arg1, arg2) {
+    if(typeof(arg1) !== "number" || typeof(arg2) !== "number" || arg1 == arg2 || arg1 < 0 || arg2 < 0){
+        return "ERROR"
+    }else{
+        let i;
+        let end;
+        let total = 0;
+        i = (arg1 < arg2) ? arg1 : arg2;
+        end = (arg1 < arg2) ? arg2 : arg1;
+        while(i <= end){
+            total += i++;
+        }
+        return total
+    }
 };
 
 // Do not edit below this line
